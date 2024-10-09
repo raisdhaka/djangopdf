9292edf2e4b0d84d714560e21e425b9b9a93d202
root <root@DESKTOP-DQFCT8H>
2024-10-09 13:19:04 +0600

    ss

--- customer_invoice/invoices/urls.py
+++ customer_invoice/invoices/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.print_invoice, name='print_invoice'),
+    path('', views.index, name='index'),
+    path('schedule1/', views.schedule1, name='schedule1'),
+     path('schedule5/', views.schedule5, name='schedule5'),
     path('itTenB/', views.print_itTenB, name='itTenB'),
 ]
--- customer_invoice/invoices/views.py
+++ customer_invoice/invoices/views.py
@@ -4,93 +4,61 @@ from .models import Invoice, InvoiceItem
 from xhtml2pdf import pisa
 from django.http import HttpResponse
 from io import BytesIO
+from django.template.loader import get_template
 
 
-def print_invoice(request):
+def index(request):
+    return render(request, 'index.html')
 
-    if request.method == 'POST':
-        company_name = 'wfd'
-        invoice_number="234345"
-        invoice_date="2024/09/28"
-        customer_name="Beku"
-        customer_address="Cherag Ali"
-        item="socuso ocoo"
-        # Generate PDF using xhtml2pdf
-        result = generate_pdf_function(company_name, invoice_number, invoice_date, customer_name, customer_address, item)
-        if result.err:
-            return HttpResponse('Error generating PDF: %s' % result.err)
-        response = HttpResponse(content_type='application/pdf')
-        response.write(result.dest.getvalue())
-        return response
+def schedule1(request):
+    context = {
+            "company_name" : "wfd",
+            "invoice_number":"234345",
+            "invoice_date":"2024/09/28",
+            "customer_name":"Beku",
+            "customer_address":"Cherag Ali",
+            "item":"socuso ocoo",
+            "title": "Schedule 1"
+        }
+    response = render_to_pdf("schedule1.html", context)
 
 
-    invoice = Invoice.objects.get(id=1)  # Replace with the actual invoice ID
-    items = InvoiceItem.objects.filter(invoice=invoice)
-    return render(request, 'invoice.html', {'invoice': invoice, 'items': items})
+    if response.status_code == 404:
+        raise HTTP404("Invoice not found")
+
+    filename = f"schedule1.pdf"
+    content = f"attachment; filename={filename}"
+    response["Content-Disposition"] = content
+
+    return response
 
 
-def generate_pdf_function(company_name, invoice_number, invoice_date, customer_name, customer_address, item):
-    
-
-    html = '<html><body>'
 
 
-    html += '<h1 style="text-align:center">Schedule 1</h1>'
-    html += '<p style="text-align:center">The schedule below shall be filled in if there is income from emplyment</p>'
-    html += '<p>a. This part is applicable for employees receiving Salary under Govt. pay scale</p>'
-    html += '<p>Name of the Assessee:						#REF!</p>'
-    html += '<p style="text-align:right">TIN ..............................</p>'
-    
-    html += '<table style="border: 1px solid #000">'
-    html += '<thead><tr><th width="20">Sl</th><th width="60%">Particulars</th><th>Income Amount</th><th>Tax Exempted Incoome</th><th>Net Taxable Income</th></tr></thead>'
-    html += '<tbody>'
-    html += '<tr><td>1</td><td>Basic pay</td><td></td><td></td></tr>'
-    html += '<tr><td>2</td><td>Arrear Salary (if not included in taxable income earlier)</td><td></td><td></td></tr>'
-    html += '<tr><td>3</td><td>Special allowance / Honorarium</td><td></td><td></td></tr>'
-    html += '<tr><td>4</td><td>House rent allowance / Telephone</td><td></td><td></td></tr>'
-    html += '<tr><td>5</td><td>Medical allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>6</td><td>Conveyance allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>7</td><td>Festival Allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>8</td><td>Allowance for support staff</td><td></td><td></td></tr>'
-    html += '<tr><td>9</td><td>Telephone Allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>10</td><td>Mobile Allowance Allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>11</td><td>Recreation  allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>12</td><td>Baishakhi Allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>13</td><td>Interest accrued on provident fund</td><td></td><td></td></tr>'
-    html += '<tr><td>14</td><td>Entertainment Allowance</td><td></td><td></td></tr>'
-    html += '<tr><td>15</td><td>Gratuity</td><td></td><td></td></tr>'
-    html += '<tr><td>16</td><td>Others, if any (Education Allowance)</td><td></td><td></td></tr>'
-    html += '<tr><td>17</td><td>Total </td><td></td><td></td></tr>'
-    html += '</tbody></table>'
-
-    html += '<p>b. This part is applicable for employees other than employees receiving Salary under Govt. pay scale</p>'
-    
-    html += '<table style="border: 1px solid #000">'
-    html += '<thead><tr><th width="20">Sl</th><th width="70%">Particulars</th><th>Income Amount</th><th>Income Amount</th></tr></thead>'
-    html += '<tbody>'
-    
-    html += '<tr><td>1</td><td>Basic pay<td></td><td></td></tr>'
-    html += '<tr><td>2</td><td>Allowances<td></td><td></td></tr>'
-    html += '<tr><td>3</td><td>Advance / Arrear Salary<td></td><td></td></tr>'
-    html += '<tr><td>4</td><td>Gratuity, Annuity, Pension or similar benefits<td></td><td></td></tr>'
-    html += '<tr><td>5</td><td>Perquisites<td></td><td></td></tr>'
-    html += '<tr><td>6</td><td>Receipt in lieu of or in addition to salary or wages<td></td><td></td></tr>'
-    html += '<tr><td>7</td><td>Income from Employees share scheme<td></td><td></td></tr>'
-    html += '<tr><td>8</td><td>Accomodation benefits<td></td><td></td></tr>'
-    html += '<tr><td>9</td><td>Car benefits<td></td><td></td></tr>'
-    html += '<tr><td>10</td><td>Any other benefits provided by employer<td></td><td></td></tr>'
-    html += '<tr><td>11</td><td>Employers contribution to recognized provident fund<td></td><td></td></tr>'
-    html += '<tr><td>12</td><td>Others, if any ( provide particulars)<td></td><td></td></tr>'
-    html += '<tr><td>13</td><td>Total salary received  (aggregate of 1 to 12)<td></td><td></td></tr>'
-    html += '<tr><td>14</td><td>Exempted Portion (As per Part 1 of 6th scheduleof  ITA 2023)<td></td><td></td></tr>'
-    html += '<tr><td>15</td><td>Total Income from Employment (13-14)<td></td><td></td></tr>'
-    html += '</tbody></table>'
+def schedule5(request):
+    context = {
+            "company_name" : "wfd",
+            "invoice_number":"234345",
+            "invoice_date":"2024/09/28",
+            "customer_name":"Beku",
+            "customer_address":"Cherag Ali",
+            "item":"socuso ocoo",
+            "title": "Schedule 5"
+        }
+    response = render_to_pdf("schedule5.html", context)
 
 
-  
-    html += '</body></html>'
-    result = pisa.CreatePDF(html, dest=BytesIO())
-    return result
+    if response.status_code == 404:
+        raise HTTP404("Invoice not found")
+
+    filename = f"schedule5.pdf"
+    content = f"attachment; filename={filename}"
+    response["Content-Disposition"] = content
+
+    return response
+
+
+
 
 
 def itTenB_pdf_function():
@@ -187,6 +155,7 @@ def itTenB_pdf_function():
 
   
     html += '</body></html>'
+    
     result = pisa.CreatePDF(html)
     return result
 
@@ -207,3 +176,11 @@ def print_itTenB(request):
     return render(request, 'itTenB.html')
 
     
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if pdf.err:
+        return HttpResponse("Invalid PDF", status_code=400, content_type='text/plain')
+    return HttpResponse(result.getvalue(), content_type='application/pdf')
